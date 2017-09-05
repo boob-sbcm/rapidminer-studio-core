d21bea76e5892c512f7cf6ebd6491044e6fb9595
长毛是狗总工程师 <13788122944@qq.com>
2017-09-05 17:32:05 +0800

    转化类为html文档

--- src/main/java/CliLauncher.java
+++ src/main/java/CliLauncher.java
@@ -6,7 +6,7 @@ import com.rapidminer.tools.PlatformUtilities;
 class CliLauncher {
     public static void main(String args[]) throws Exception {
         System.setProperty("com.apple.macos.useScreenMenuBar","true");
-        System.setProperty("apple.laf.useScreenMenuBar", "true")
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty(PlatformUtilities.PROPERTY_RAPIDMINER_HOME, Paths.get("").toAbsolutePath().toString());
         RapidMiner.setExecutionMode(RapidMiner.ExecutionMode.COMMAND_LINE);
         RapidMiner.init();
